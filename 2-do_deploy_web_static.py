81e4f62434a4e4614fed6d882f0a9a3930006e9c
Tebogo Ndaba <dennistebogo@gmail.com>
2023-12-08 01:26:58 +0200

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,36 +13,36 @@ env.user = 'ubuntu'
 def do_deploy(archive_path):
     """Distributes an archive to the web servers and deploys it."""
 
+    # Check if the archive file exists
     if not exists(archive_path):
         return False
 
     try:
-        # Extract name without extension from the archive path
-        archive_name = archive_path.split('/')[-1]
-        archive__name = archive_name.split('.')[0]
-
         # Upload the archive to /tmp/ directory on the web server
-        put(archive_path, '/tmp')
+        put(archive_path, '/tmp/')
 
-        # Create necessary folders and extract the archive
-        run('sudo mkdir -p /data/web_static/releases/{}/'.format(archive_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-            archive__name, archive_name))
+        # Extract the archive to /data/web_static/releases/ folder
+        archive_filename = archive_path.split('/')[-1]
+        archive_folder = '/data/web_static/releases/{}'.format(
+            archive_filename.split('.')[0]
+        )
+        run('mkdir -p {}'.format(archive_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, archive_folder))
 
         # Remove the uploaded archive from the web server
-        run('rm /tmp/{}.tgz'.format(archive__name))
+        run('rm /tmp/{}'.format(archive_filename))
 
-        # Move contents and update symbolic link
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(archive_name, archive_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            archive_name))
+        # Move the contents of the extracted archive to the web_static folder
+        run('mv {}/web_static/* {}'.format(archive_folder, archive_folder))
+
+        # Remove the old symbolic link
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(archive_name))
 
-        print("New version deployed!")
+        # Create a new symbolic link
+        run('ln -s {} /data/web_static/current'.format(archive_folder))
+
+        print('New version deployed!')
         return True
 
-    except:
+    except Exception:
         return False
