ea1458f53bdaf21a483e0b46a7de2c12b27c4c7a
Tebogo Ndaba <dennistebogo@gmail.com>
2023-12-08 01:01:55 +0200

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,9 +2,8 @@
 """ A Fabric script that generates a .tgz archive. """
 
 
-from fabric.api import local, put, run, env
-from os.path import exists, join, isfile
-from datetime import datetime
+from fabric.api import put, run, env
+from os.path import exists
 
 
 env.hosts = ['54.237.94.18', '100.26.154.55']
@@ -14,22 +13,23 @@ env.user = 'ubuntu'
 def do_deploy(archive_path):
     """Distributes an archive to the web servers and deploys it."""
 
-    if not isfile(archive_path):
+    if not exists(archive_path):
         return False
 
     try:
         # Extract name without extension from the archive path
-        archive_name = archive_path.split('/')[-1][:-4]
+        archive_name = archive_path.split('/')[-1]
+        archive__name = archive_name.split('.')[0]
 
         # Upload the archive to /tmp/ directory on the web server
         put(archive_path, '/tmp')
 
         # Create necessary folders and extract the archive
         run('sudo mkdir -p /data/web_static/releases/{}/'.format(archive_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(archive_name, archive_name))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(archive__name, archive_name))
 
         # Remove the uploaded archive from the web server
-        run('rm /tmp/{}.tgz'.format(archive_name))
+        run('rm /tmp/{}.tgz'.format(archive__name))
 
         # Move contents and update symbolic link
         run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_name, archive_name))
