369c9b701a0489ce789b0dcafd96c2adbef9b130
Tebogo Ndaba <dennistebogo@gmail.com>
2023-12-08 00:04:14 +0200

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """ A Fabric script that generates a .tgz archive. """
 
 
-from fabric.api import local
+from fabriapi import local, put, run, path
 from os.path import exists, join
 from datetime import datetime
 
@@ -34,7 +34,7 @@ def do_pack():
         return None
 
 
-def do_deploy(c, archive_path):
+def do_deploy(archive_path):
     """Distributes an archive to the web servers and deploys it."""
 
     # Check if the archive file exists
@@ -43,27 +43,27 @@ def do_deploy(c, archive_path):
 
     try:
         # Upload the archive to /tmp/ directory on the web server
-        c.put(archive_path, '/tmp/')
+        put(archive_path, '/tmp/')
 
         # Extract the archive to /data/web_static/releases/ folder
         archive_filename = archive_path.split('/')[-1]
         archive_folder = '/data/web_static/releases/{}'.format(
             archive_filename.split('.')[0]
         )
-        c.run('mkdir -p {}'.format(archive_folder))
-        c.run('tar -xzf /tmp/{} -C {}'.format(archive_filename, archive_folder))
+        run('mkdir -p {}'.format(archive_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, archive_folder))
 
         # Remove the uploaded archive from the web server
-        c.run('rm /tmp/{}'.format(archive_filename))
+        run('rm /tmp/{}'.format(archive_filename))
 
         # Move the contents of the extracted archive to the web_static folder
-        c.run('mv {}/web_static/* {}'.format(archive_folder, archive_folder))
+        run('mv {}/web_static/* {}'.format(archive_folder, archive_folder))
 
         # Remove the old symbolic link
-        c.run('rm -rf /data/web_static/current')
+        run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link
-        c.run('ln -s {} /data/web_static/current'.format(archive_folder))
+        run('ln -s {} /data/web_static/current'.format(archive_folder))
 
         print('New version deployed!')
         return True
